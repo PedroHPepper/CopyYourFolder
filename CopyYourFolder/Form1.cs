9bf7d2e34f4e8c52dbdb0f505021a03fb37da2b7
Pedro Pimenta <pedropimenta@rocketmail.com>
2021-03-05 16:22:09 -0300

    First commit

---
+++ CopyYourFolder/Form1.cs
@@ -0,0 +1,94 @@
+﻿using CopyYourFolder.Libs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyYourFolder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Escolha o diretório" })
+            {
+                if(fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Escolha o diretório" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox2.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                const string message =
+                    "Favor, selecione os diretórios de origem e de destino para transferência de arquivos.";
+                const string caption = "Erro";
+                MessageBox.Show(message, caption,
+                                             MessageBoxButtons.OK);
+            }else if(textBox1.Text == textBox2.Text)
+            {
+                const string message =
+                    "Os diretórios devem ser diferentes!";
+                const string caption = "Erro";
+                MessageBox.Show(message, caption,
+                                             MessageBoxButtons.OK);
+            }
+            else
+            {
+                Copy.DirectoryCopy(textBox1.Text, textBox2.Text, true);
+            }
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CopyYourFolder/Libs/Copy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CopyYourFolder.Libs
+{
+    public class Copy
+    {
+        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            var dir = new DirectoryInfo(sourceDirName);
+            var DirSubDirs = dir.GetDirectories();
+
+            // If the source directory does not exist, throw an exception.
+            if (!dir.Exists)
+            {
+                const string message =
+                    "Não foi possível encontrar o diretório de origem.";
+                const string caption = "Erro";
+                MessageBox.Show(message, caption,
+                                             MessageBoxButtons.OK);
+                return;
+            }
+
+            // If the destination directory does not exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the file contents of the directory to copy.
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                // Create the path to the new copy of the file.
+                var temppath = Path.Combine(destDirName, file.Name);
+                if (File.Exists(temppath))
+                {
+                    string message =
+                    "Já foi encontrado um arquivo com o nome de "+ file.Name + ". Deseja substituí-lo?";
+                    const string caption = "Erro";
+                    var result = MessageBox.Show(message, caption,
+                                                 MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        File.Delete(temppath);
+                        file.CopyTo(temppath, true);
+                    }
+                }
+                else
+                {
+                    // Copy the file.
+                    file.CopyTo(temppath, true);
+                }
+                
+
+            }
+
+            // If copySubDirs is true, copy the subdirectories.
+            if (!copySubDirs) return;
+
+            foreach (var subdir in DirSubDirs)
+            {
+                // Create the subdirectory.
+                var temppath = Path.Combine(destDirName, subdir.Name);
+
+                // Copy the subdirectories.
+                DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+            }
+        }
+    }
+}
